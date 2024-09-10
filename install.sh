8f37d828e8729e3398816ee254350ade5f2e0068
MoziL <57535353+simozil@users.noreply.github.com>
2024-09-10 22:22:05 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 
-echo "Showing HCA logo..."
-wget -O loader.sh https://raw.githubusercontent.com/DiscoverMyself/Ramanode-Guides/main/loader.sh && chmod +x loader.sh && ./loader.sh
-curl -s https://raw.githubusercontent.com/DiscoverMyself/Ramanode-Guides/main/logo.sh | bash
-sleep 2
-
 echo "Welcome to the t3rn Executor Setup by Happy Cuan Airdrop!"
+sleep 2
 
 cd $HOME
 sudo apt -q update
